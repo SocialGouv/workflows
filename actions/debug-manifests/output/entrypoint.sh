de2c1ab1c6c50b477a60bb2edf23839ec536e10b
devthejo <jo@surikat.pro>
2023-04-17 18:25:40 +0200

    chore: wip

--- actions/debug-manifests/output/entrypoint.sh
+++ actions/debug-manifests/output/entrypoint.sh
@@ -14,9 +14,8 @@ MARKDOWN_JSON=$(jo result="$MARKDOWN")
 
 echo "markdown-json=$MARKDOWN_JSON" >> $GITHUB_OUTPUT
 
-manifests_images=$(echo $JSON | jq .images)
+manifests_images=$(echo $JSON | jq -c .images)
 
-echo "manifests-images=$manifests_images"
 echo "manifests-images=$manifests_images" >> $GITHUB_OUTPUT
 
 echo "$MARKDOWN" >> $GITHUB_STEP_SUMMARY
